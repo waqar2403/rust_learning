2756034080332efbd1018592b4a690f237697a15
Waqar <waqar216447@gmail.com>
2025-03-19 00:22:46 +0500

    Here we again git add .!

--- Basics/guessing-game/src/main.rs
+++ Basics/guessing-game/src/main.rs
@@ -1,8 +1,26 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 fn main() {
-    println!("Enter a number :");
+    let sec = rand::thread_rng().gen_range(1..=100);
+
+loop{
+    println!("Guess a number (1-100) :");
+
     let mut g = String::new();
     io::stdin().read_line(&mut g).expect("Input Failed !!!");
+
+    let g : u32 = match g.trim().parse(){
+        Ok(num) => num,
+        Err(_) => continue,
+    };
+    
     println!("You Guessed {}",g);
     
+    match g.cmp(&sec){
+        Ordering::Less => println!{"To Low :("},
+        Ordering::Greater => println!{"To High :("},
+        Ordering::Equal => {println!("Is Equal :)");break;},
+    }
+    } 
 }
